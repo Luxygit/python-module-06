b6afbdd68d9463a2a949b49bc52e8d816a07d3a0
Diego Vargas Romero <dievarga@car5s2.42barcelona.com>
2026-07-02 11:40:50 +0200

    sdf

--- alchemy/grimoire/dark_spellbook.py
+++ alchemy/grimoire/dark_spellbook.py
@@ -8,7 +8,7 @@ def dark_spell_allowed_ingredients() -> list[str]:
     return ["bats", "frogs", "arsenic", "eyeball"]
 
 
-def darl_spell_record(spell_name: str, ingredients: str) -> str:
+def dark_spell_record(spell_name: str, ingredients: str) -> str:
     """tries to evaluate and log a darkspell"""
     val_string = validate_ingredients(ingredients)
-    return  f"Spell recorded: {spell_name} ({val_string})"
+    return f"Spell recorded: {spell_name} ({val_string})"
--- ft_alembic_4.py
+++ ft_alembic_4.py
@@ -4,9 +4,9 @@ import alchemy
 if __name__ == "__main__":
     print("=== Alembic 4 ===")
     print("Accessing the alchemy module using 'import alchemy'")
-    res = alchemy.create_air()
+    res = alchemy.create_air()  # type: ignore
     print(f"Testing create_air: {res}")
     print("Now show that not all functions can be reached")
     print("This will raise an exception!")
     print("Testing the hidden create_earth: ", end="")
-    print(f"{alchemy.create_earth()}")
+    print(f"{alchemy.create_earth()}")  # type: ignore
